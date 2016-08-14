6072c314682ebd37ca96403e0a01f6813f830d8a
karlmorand <karljmorand@gmail.com>
2016-08-13 22:21:19 -0400

    setup index route and create route for workouts

---
+++ app/controllers/workouts_controller.rb
@@ -0,0 +1,24 @@
+class WorkoutsController < ApplicationController
+  def index
+    @workouts = Workout.all
+  end
+
+  def new
+    @workout = Workout.new
+  end
+
+  def create
+    @workout = Workout.new(workout_params)
+    if @workout.save
+      redirect_to 'workouts_url'
+    else
+      redirect_to 'new_workout_url'
+    end
+  end
+
+
+private
+  def workout_params
+    params.require(:workout).permit(:title, :description)
+  end
+end
