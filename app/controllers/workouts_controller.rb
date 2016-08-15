774c07e20281c939f584a48dbdf510c17566e459
karlmorand <karljmorand@gmail.com>
2016-08-15 12:05:49 -0400

    created backend for show pages to display

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -16,6 +16,11 @@ class WorkoutsController < ApplicationController
     end
   end
 
+  def show
+    @workout = Workout.find(params[:id])
+
+  end
+
 
 private
   def workout_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/workouts', to: 'workouts#index'
-  get '/workouts/new', to: 'workouts#new'
-  post '/workouts', to: 'workouts#create'
-
+  resources :workouts
+  resources :session
 
 end
